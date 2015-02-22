7b8e896ccbc32fffabd2b778608492e0c472e35a
Tanner Welsh <tannerwelsh@gmail.com>
2015-02-21 22:56:32 -0500

    Use homespun flash implementation, not rack-flash
    get_flash(key) retrieves a value from within session[:flash]
    set_flash(key, value) sets a value in the session[:flash] hash

--- flash-messages/app-flash-messages.rb
+++ flash-messages/app-flash-messages.rb
@@ -6,17 +6,22 @@ require "./setup"
 # models.rb is where we define our Chirp model
 require "./models"
 
-# To use flash messages, we need to require the Flash middleware
-require "rack-flash"
-
 # Because flash middleware uses sessions, we need to enable sessions
 set(:sessions, true)
 
 # Make sure to use a session secret!
 set(:session_secret, ENV["SESSION_SECRET"])
 
-# Finally, to enable the `flash` method we add Rack::Flash to our middleware
-use Rack::Flash
+helpers do
+  def get_flash(key)
+    session[:flash] && session[:flash].delete(key)
+  end
+
+  def set_flash(key, value)
+    session[:flash] ||= {}
+    session[:flash][key] = value
+  end
+end
 
 get("/") do
   chirps = Chirp.all
@@ -33,12 +38,12 @@ post("/chirps") do
 
   if chirp.saved?
     # When the chirp is saved successfully, show a nice congratulatory message!
-    flash[:notice] = "New chirp created!"
+    set_flash(:notice, "New chirp created!")
     redirect("/")
   else
     # If the chirp wasn't saved, add the error messages to flash so that the
     # user can correct their mistakes the next time around.
-    flash[:errors] = chirp.errors.full_messages
+    set_flash(:errors, chirp.errors.full_messages)
     redirect("/chirps/new")
   end
 end
